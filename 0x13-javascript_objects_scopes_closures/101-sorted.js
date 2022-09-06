90c1cc8add635191363e0b8953e3df645c8a0fb9
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-09-06 23:06:52 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
 let newDict = {};
-for (let key of dict) {
-  if (newDict[key] === undefined) {
-    newDict[key] = [key];
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
   } else {
-    newDict[key].push(key);
+    newDict[dict[key]].push(key);
   }
 }
 console.log(newDict);
